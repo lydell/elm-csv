6c69b10c399ab0a3d86bda6e64db55fb2db7efdb
Simon Lydell <simon.lydell@gmail.com>
2021-01-22 18:22:49 +0100

    Try running elm-tooling install

--- ci.sh
+++ ci.sh
@@ -2,7 +2,7 @@
 #!nix-shell --pure -i bash
 set -euo pipefail
 
-export ELM_HOME=elm-stuff/elm-home
+# export ELM_HOME=elm-stuff/elm-home
 
 # tests
 # elm-verify-examples
@@ -13,6 +13,8 @@ export ELM_HOME=elm-stuff/elm-home
 
 # linting
 # elm-format --validate src
+
+elm-tooling/index.js install
 
 # elm-review tries to download elm-json, and it fails in CI. We'll try again
 # in the 20.05 release of Nix, where it's packaged natively.
