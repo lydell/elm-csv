741f1202a0850dbcbf0fbeaa5db16034cf54ba9c
Simon Lydell <simon.lydell@gmail.com>
2021-01-22 17:44:27 +0100

    Disable everything except elm-review

--- ci.sh
+++ ci.sh
@@ -3,14 +3,14 @@
 set -euo pipefail
 
 # tests
-elm-verify-examples
-elm-test
+# elm-verify-examples
+# elm-test
 
 # docs
-elm make --docs=documentation.json
+# elm make --docs=documentation.json
 
 # linting
-elm-format --validate src
+# elm-format --validate src
 
 # elm-review tries to download elm-json, and it fails in CI. We'll try again
 # in the 20.05 release of Nix, where it's packaged natively.
